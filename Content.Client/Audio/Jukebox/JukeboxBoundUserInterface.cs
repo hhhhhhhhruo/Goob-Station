82db94b54f75b9c343035ce08a86d4e677441263
Scape <70858087+Masterscape@users.noreply.github.com>
2025-05-18 19:44:59 +0300

    Port New Jukebox Features: Shuffling & Replaying Songs

--- Content.Client/Audio/Jukebox/JukeboxBoundUserInterface.cs
+++ Content.Client/Audio/Jukebox/JukeboxBoundUserInterface.cs
@@ -51,6 +51,13 @@ public sealed class JukeboxBoundUserInterface : BoundUserInterface
 
         _menu.OnSongSelected += SelectSong;
 
+        // Frontier: Shuffle & Repeat
+        _menu.OnModeChanged += playbackMode =>
+        {
+            SendMessage(new JukeboxSetPlaybackModeMessage(playbackMode));
+        };
+        // End Frontier: Shuffle & Repeat
+
         _menu.SetTime += SetTime;
         PopulateMusic();
         Reload();
@@ -105,4 +112,12 @@ public sealed class JukeboxBoundUserInterface : BoundUserInterface
 
         SendMessage(new JukeboxSetTimeMessage(sentTime));
     }
+
+    // Frontier: Shuffle & Repeat
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+        _menu?.UpdateState(state);
+    }
+    // End Frontier
 }
--- Content.Shared/Audio/Jukebox/SharedJukeboxSystem.cs
+++ Content.Shared/Audio/Jukebox/SharedJukeboxSystem.cs
@@ -5,6 +5,7 @@
 // SPDX-License-Identifier: MIT
 
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Serialization; // Frontier
 
 namespace Content.Shared.Audio.Jukebox;
 
@@ -12,3 +13,11 @@ public abstract class SharedJukeboxSystem : EntitySystem
 {
     [Dependency] protected readonly SharedAudioSystem Audio = default!;
 }
+
+// Frontier: Shuffle & Repeat
+[Serializable, NetSerializable]
+public sealed class JukeboxInterfaceState(JukeboxPlaybackMode playbackMode) : BoundUserInterfaceState
+{
+    public JukeboxPlaybackMode PlaybackMode { get; set; } = playbackMode;
+}
+// End Frontier: Shuffle & Repeat
